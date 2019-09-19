a298e3219e616be7cafa6fa5bcc1a718ee09f702
Vivihhw <huhu6404@uni.sydney.edu.au>
2019-09-19 15:19:43 +1000

    Update validation set

--- cache_train.py
+++ cache_train.py
@@ -17,21 +17,25 @@ data_path = '../data'
 gs = pd.read_csv(os.path.join(data_path, 'grid_sizes.csv'), names=['ImageId', 'Xmax', 'Ymin'], skiprows=1)
 
 shapes = pd.read_csv(os.path.join(data_path, '3_shapes.csv'))
-test_id = ['6110_3_1', '6120_2_2', '6140_3_1', '6110_1_2', '6110_4_0', '6120_2_0', '6120_2_0']
-
+train_id = ['6110_3_1', '6120_2_2', '6140_3_1', '6110_1_2', '6110_4_0']
+validation_id = ['6120_2_0']
 unlabel_id =['6010_4_0','6010_4_1','6010_4_2']
 
 def cache_train_16():
     train_wkt = pd.read_csv(os.path.join(data_path, 'train_wkt_v4.csv'))
-    train_wkt = train_wkt[~train_wkt['ImageId'].isin(test_id)]
+    sample_wkt = train_wkt[train_wkt['ImageId'].isin(train_id+validation_id+unlabel_id)]
+    train_wkt = train_wkt[train_wkt['ImageId'].isin(train_id)]
 
     print('num_label_train_images =', train_wkt['ImageId'].nunique())
     print('num_unlabel_train_images =', len(unlabel_id))
+    print('num_validation_images =', len(validation_id))
 
     train_shapes = shapes[shapes['image_id'].isin(train_wkt['ImageId'].unique())]
+    sample_shapes = shapes[shapes['image_id'].isin(sample_wkt['ImageId'].unique())]
+    min_train_height = sample_shapes['height'].min()
+    min_train_width = sample_shapes['width'].min()
 
-    min_train_height = train_shapes['height'].min()
-    min_train_width = train_shapes['width'].min()
+
 
     num_train = train_shapes.shape[0]
 
@@ -46,13 +50,17 @@ def cache_train_16():
 
     f = h5py.File(os.path.join(data_path, 'train_label.h5'), 'w', compression='blosc:lz4', compression_opts=9)
     f_unlabel = h5py.File(os.path.join(data_path, 'train_unlabel.h5'), 'w', compression='blosc:lz4', compression_opts=9)
+    f_validation = h5py.File(os.path.join(data_path, 'validation.h5'), 'w', compression='blosc:lz4', compression_opts=9)
     imgs_unlabel = f_unlabel.create_dataset('train', (num_unlabeltrain, num_channels, image_rows, image_cols), dtype=np.float16)
 
     imgs = f.create_dataset('train', (num_train, num_channels, image_rows, image_cols), dtype=np.float16)
     imgs_mask = f.create_dataset('train_mask', (num_train, num_mask_channels, image_rows, image_cols), dtype=np.uint8)
 
+    imgs_validation = f_validation.create_dataset('train', (num_unlabeltrain, num_channels, image_rows, image_cols), dtype=np.float16)
+
     ids = []
     unlabel_ids=[]
+    validation_ids=[]
 
     i = 0
     for image_id in tqdm(sorted(train_wkt['ImageId'].unique())):
@@ -82,6 +90,16 @@ def cache_train_16():
         i += 1
     f_unlabel['train_ids'] = np.array(unlabel_ids).astype('|S9')
     f_unlabel.close()
+
+    i = 0
+    for image_id in tqdm(validation_id):
+        image = tiff.imread("../data/three_band/{}.tif".format(image_id)) / 2047.0
+        _, height, width = image.shape
+        imgs_validation[i] = image[:, :min_train_height, :min_train_width]
+        validation_ids += [image_id]
+        i += 1
+    f_validation['validation_ids'] = np.array(validation_ids).astype('|S9')
+    f_validation.close()
 
 
 if __name__ == '__main__':
